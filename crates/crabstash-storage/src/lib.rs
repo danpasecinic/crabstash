808207f2f4cca1a2d267d1de9349fa531492001b
danpasecinic <dan@withlocals.com>
2025-12-21 16:50:44 +0100

    feat: implement block caching support with moka

--- crates/crabstash-storage/src/lib.rs
+++ crates/crabstash-storage/src/lib.rs
@@ -1,4 +1,5 @@
 mod bloom;
+mod cache;
 mod compaction;
 pub mod iterator;
 mod lsm;
@@ -7,6 +8,7 @@ mod memtable;
 mod sstable;
 mod wal;
 
+pub use cache::BlockCache;
 pub use iterator::{BoundedIterator, MergeIterator, StorageIterator, TwoMergeIterator};
 pub use lsm::{Lsm, LsmIterator, LsmOptions};
 pub use memtable::{MemTable, MemTableIterator};
